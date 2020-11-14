dea14f0f77d6db28c370cb4d5729d21e7167868e
Parthiv Kativarapu <parthiv.kativarapu@ideas.com>
2020-11-14 21:05:29 +0530

    What? - Added colors for various timings

--- src/app/bgcolor.service.ts
+++ src/app/bgcolor.service.ts
@@ -7,14 +7,13 @@ export class BgcolorService {
 
   constructor() { }
 
-  midNight: string[] = [];
+  midNight: string[] = ["#072549", "#000207"];
   sunRise: string[] = ["#6E4679", "#FA693A"];
-  morning: string[] = [];
-  noon: string[] = [];
+  morning: string[] = ["#DDB5FC", "#FCAD98"];
   afternoon: string[] = ["#6AA6D8", "#F0AA90"];
   evening: string[] = ["#832212", "#DBB353"];
   sunset: string[] = ["#480F56", "#CC728A"];
-  night: string[] = [];
+  night: string[] = ["#19164F", "#6A4A86"];
 
   
 
